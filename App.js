63d3e4a9a25b7568a9bbbd70e48b3f5799443244
jinsongeroge <jinsongeorge62@gmail.com>
2022-03-23 16:27:21 +0530

    Register page design created

--- App.js
+++ App.js
@@ -1,112 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Register from './src/Register'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Stack = createStackNavigator();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function MyStack() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Register'
+        component={Register}
+        options={{headerShown:false}}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+            />
+    </Stack.Navigator>
+  )
+}
+export default function App() {
+  return(
+      <NavigationContainer>
+    <MyStack />
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  </NavigationContainer>
+  )
+}
---
+++ src/Register.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Image, TextInput, TouchableHighlight } from 'react-native'
+export default class Register extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={require('../src/assets/logo.png')}
+                    style={styles.logo}>
+                </Image>
+                <Text style={styles.text1}>
+                    Enter your mobile number
+                </Text>
+
+                <Text style={styles.txt2}>
+                    We will send you an OTP to verify.
+                </Text>
+                <View style={styles.view1}>
+                    <View style={styles.view2}>
+                        <TextInput style={styles.inputbox1}
+                            placeholder='+91'
+                            editable={false}
+
+                            placeholderTextColor={'white'}
+                        >
+                        </TextInput>
+                        <TextInput style={styles.inputbox2}
+                            maxLength={10}
+                            placeholder='Phone number'
+                            placeholderTextColor={'#446270'}
+                            editable={true}
+                        >
+                        </TextInput>
+
+                    </View>
+                    <TouchableHighlight style={styles.touchable}>
+                        <Text style={styles.touchbletxt}>
+                            Continue
+                        </Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.compose({
+    container: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    logo: {
+        width: 253,
+        height: 253,
+        resizeMode: "contain",
+        marginTop: 80,
+        backgroundColor: 'white'
+    },
+    text1: {
+        color: "#002333",
+        fontSize: 20,
+        fontFamily: 'Gilroy',
+        fontWeight: '700',
+        marginTop: 50
+    },
+    txt2: {
+        color: '#9F9F9F',
+        fontFamily: 'Gilroy-Regular',
+        fontSize: 14,
+
+    }, view1: {
+        flexDirection: 'column',
+        width: '100%',
+        height: 300,
+        backgroundColor: '#002333',
+        borderRadius: 16,
+        marginTop: 32
+    },
+    view2: {
+        flexDirection: 'row',
+
+    },
+    inputbox1: {
+        width: 59,
+        height: 51,
+        borderColor: '#13394A',
+        borderWidth: 1,
+        marginTop: 32,
+        marginLeft: 32,
+        backgroundColor: '#062E40',
+        paddingLeft: 15
+    },
+    inputbox2: {
+        width: 244,
+        height: 51,
+        borderColor: '#13394A',
+        borderWidth: 1,
+        marginTop: 32,
+        marginLeft: 32,
+        backgroundColor: '#062E40',
+        paddingLeft: 15
+    },
+    touchable: {
+        width: 311,
+        height: 56,
+        backgroundColor: '#00C458',
+        marginLeft: 45,
+        borderRadius: 8,
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    touchbletxt: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontFamily: 'Gilroy',
+        fontWeight: '700',
+    }
+})
